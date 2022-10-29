23382757b689599f08bd58989bbdb7501e04c9da
Krzysztof Krej <kris@krej.net>
2022-10-29 10:08:51 +0200

    Unity update, scene simplification

---
+++ Assets/Breather.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Breather : MonoBehaviour {
+    public List<AudioClip> mp3s = new List<AudioClip>();
+    public AudioSource audioSource;
+    
+    
+    
+    void Update() {
+        if (!audioSource.isPlaying)
+            PlayRandomMp3();
+    }
+
+    void PlayRandomMp3() {
+        audioSource.PlayOneShot( SelectRandomMp3() );
+        audioSource.volume = Random.Range(0, 2);
+    }
+
+    AudioClip SelectRandomMp3() {
+        var id = Random.Range(0, mp3s.Count);
+        return mp3s[id];
+    }
+}
