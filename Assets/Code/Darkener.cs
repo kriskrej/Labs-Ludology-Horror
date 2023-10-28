2b326fbe4127aeb6275cf59561e87f45d52f65d9
Kris Krej <kris@krej.net>
2023-10-28 10:53:48 +0200

    updated to URP

--- Assets/Code/Darkener.cs
+++ Assets/Code/Darkener.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
 
 public class Darkener : MonoBehaviour {
-    PostProcessVolume volume;
-    ColorGrading colorGrading;
-    Vignette vignette;
+    [SerializeField] Volume volume;
+    //ColorGrading colorGrading;
+    //Vignette vignette;
     public static Darkener instance;
     
     void Awake() {
@@ -20,52 +20,31 @@ public class Darkener : MonoBehaviour {
 
     IEnumerator ShockThePlayerCoroutine() {
         SetVolumeEnabledAndInvisible();
-        yield return TransitVignetIntensityToOne(0.3f);
-        yield return ContrastToLowest(0.1f);
-        yield return BackToNeutral(0.3f);
+        yield return TweenWeightToOne(0.3f);
+        yield return TweenWeightToZero(0.3f);
         SetVolumeDisabled();
-
     }
 
     void SetVolumeEnabledAndInvisible() {
-        if (volume == null) volume = GetComponent<PostProcessVolume>();
         volume.enabled = true;
-        
-        volume.profile.TryGetSettings(out colorGrading);
-        volume.profile.TryGetSettings(out vignette);
-        colorGrading.enabled.value = true;
-        vignette.enabled.value = true;
-        colorGrading.contrast.value = 0;
-        vignette.intensity.value = 0;
-        vignette.smoothness.value = 0.2f;
+        volume.weight = 0;
     }
-
-    IEnumerator TransitVignetIntensityToOne(float duration) {
-        while (vignette.intensity < 1) {
-            vignette.intensity.value += Time.deltaTime/duration;
-            vignette.smoothness.value += Time.deltaTime/duration;
-            colorGrading.contrast.value += Time.deltaTime * 100f/duration;
+    
+    IEnumerator TweenWeightToOne(float duration) {
+        for (float time = 0; time < duration; time += Time.deltaTime) {
+            volume.weight += Time.deltaTime/duration;
             yield return null;
         }
     }
     
-    IEnumerator ContrastToLowest(float duration) {
-        while (colorGrading.contrast > -100) {
-            colorGrading.contrast.value -= Time.deltaTime * 100f / duration;
-            yield return null;
-        }
-    }
-    
-    IEnumerator BackToNeutral(float duration) {
-        vignette.intensity.value = 0;
-        vignette.smoothness.value = 0.2f;
-        while (colorGrading.contrast < -0.1f) {
-            colorGrading.contrast.value += 100f*Time.deltaTime/duration; 
+    IEnumerator TweenWeightToZero(float duration) {
+        for (float time = 0; time < duration; time += Time.deltaTime) {
+            volume.weight -= Time.deltaTime/duration;
             yield return null;
         }
     }
     
     void SetVolumeDisabled() {
-        volume.enabled = false;
+        volume.weight = 0;
     }
 }
